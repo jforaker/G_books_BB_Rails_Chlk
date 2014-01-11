1f4753d6c2101b996246b4c2b12fa6823501e360
jforaker <jake@chalkable.com>
2014-01-11 11:45:09 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GBooks::Application.config.secret_key_base = 'd6ced7ba0006d3a99f053c4c9e26bad8c52e3024a8fe7e96d02024ec8170bdd7b7a8cf561994e6cca8b6b53fb1f602e40e7360bc02baf188b0cbfbdb2bc0a420'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GBooks::Application.config.session_store :cookie_store, key: '_G_Books_session'
